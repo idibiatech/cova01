0f08ce5f3b09278fbad0d7bd462e1de6e0ef0340
adeka stanley <adekastanley2@gmail.com>
2023-12-20 11:47:03 +0100

    finished navigation menu animation

---
+++ src/anim.js
@@ -0,0 +1,26 @@
+export const menuSlide = {
+	initial: {
+		x: "100%",
+	},
+	enter: {
+		x: "0%",
+		transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+	},
+	exit: {
+		x: "100%",
+		transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+	},
+};
+export const linksSlide = {
+	initial: {
+		x: "80px",
+	},
+	enter: {
+		x: "0px",
+		transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+	},
+	exit: {
+		x: "80px",
+		transition: { duration: 0.8, ease: [0.76, 0, 0.24, 1] },
+	},
+};
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,31 +1,82 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 import "./navbar.css";
 
+import { menuSlide, linksSlide } from "../anim";
+import { motion, AnimatePresence } from "framer-motion";
+
+const navLinksList = [
+	{ title: "Home", href: "/" },
+	{ title: "About", href: "/about" },
+	{ title: "Contact", href: "/contact" },
+];
 const NavBar = () => {
+	const [isActive, setIsActive] = useState(false);
 	return (
 		<div className="nav">
-			<div className="logo">
-				<Link className="nav-link" to="/">
-					Cova
-				</Link>
-			</div>
-			<div className="nav-links">
-				<div className="nav-item">
+			<div className="header-wrapper">
+				<div className="logo">
 					<Link className="nav-link" to="/">
-						Home
+						Cova
 					</Link>
 				</div>
-				<div className="nav-item">
-					<Link className="nav-link" to="/about">
-						About
-					</Link>
-				</div>
-				<div className="nav-item">
-					<Link className="nav-link" to="/contact">
-						Contact
-					</Link>
+				<div
+					className="burger-wrapper"
+					onClick={() => {
+						setIsActive(!isActive);
+					}}
+				>
+					<div
+						className={` burger ${
+							isActive ? "burgerActive" : "burgerInActive"
+						}`}
+					></div>
 				</div>
 			</div>
+			<AnimatePresence mode="wait">
+				{isActive && (
+					<motion.div
+						variants={menuSlide}
+						animate="enter"
+						initial="initial"
+						exit="exit"
+						className="nav-links"
+					>
+						<motion.div className="navHeader">NAVIGATION</motion.div>
+						{navLinksList.map((item, index) => {
+							return (
+								<motion.div
+									variants={linksSlide}
+									animate="enter"
+									initial="initial"
+									exit="exit"
+									className="nav-item"
+								>
+									<Link key={index} className="nav-link" to={item.href}>
+										{item.title}
+									</Link>
+								</motion.div>
+							);
+						})}
+						<div className="menuFooter">
+							<ul>
+								<li>
+									<a href="/">instagram</a>
+								</li>
+								<li>
+									<a href="/">twitter</a>
+								</li>
+								<li>
+									<a href="/">facebook</a>
+								</li>
+								<li>
+									<a href="/">linkedin</a>
+								</li>
+							</ul>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</div>
 	);
 };
