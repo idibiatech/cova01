326052b9a6a06f0cb4c70bf3f9a0a573f03b4afc
adeka stanley <adekastanley2@gmail.com>
2023-12-31 22:50:04 +0100

    added carousel reveal

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -2,9 +2,10 @@ import "./carousel.css";
 import ui from "../../assets/portfolio/ui.jpg";
 import branding from "../../assets/portfolio/branding.jpg";
 import logo from "../../assets/portfolio/logop.jpg";
-
 import web from "../../assets/portfolio/web.jpg";
-const imgs = [ui, branding, logo, web];
+import Reveal from "../Animated/Reveal";
+import { RevealImage } from "../Animated/Reveal";
+
 const carouselItem = [
 	{
 		img: ui,
@@ -27,10 +28,6 @@ const carouselItem = [
 		text: "    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis, natus. ",
 	},
 ];
-// "../../assets/portfolio/ui.jpg",
-// "../../assets/portfolio/branding.jpg",
-// "../../assets/portfolio/logop.jpg",
-// "../../assets/portfolio/web.jpg",
 
 const Carousel = () => {
 	return (
@@ -49,19 +46,23 @@ const Images = () => {
 			{carouselItem.map((item, index) => {
 				return (
 					<div className="carousel-item-wrapper">
-						<div
-							key={index}
-							className="carousel-Image"
-							style={{
-								backgroundImage: `url(${item.img})`,
-							}}
-						></div>
+						<RevealImage>
+							<div
+								key={index}
+								className="carousel-Image"
+								style={{
+									backgroundImage: `url(${item.img})`,
+								}}
+							></div>
+						</RevealImage>
 						<div className="carousel-text-container">
-							<h3>{item.heading}</h3>
-							<p>{item.text}</p>
-							<a href="/">
-								<div className="carousel-btn">View Portfolio</div>
-							</a>
+							<Reveal>
+								<h3>{item.heading}</h3>
+								<p>{item.text}</p>
+								<a href="/">
+									<div className="carousel-btn">View Portfolio</div>
+								</a>
+							</Reveal>
 						</div>
 					</div>
 				);
